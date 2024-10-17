5493061fe2cdd527cc77f2c47189ab7add989708
Aakash <aakashsheoran592@gmail.com>
2024-10-18 03:13:09 +0530

    modified some pages

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const FooterComponent = () => {
   return (
     <div>
-      <footer className="bg-customGrey text-white py-8">
-        <div className="container mx-0 px-4 grid gap-8 lg:grid-cols-2 sm:grid-cols-1">
-          
-          {/* Right Section (Links) - Normally below Left Section, but appears above on small screens */}
-          <div className="flex justify-between">
-            <div>
+      <footer className="bg-customGrey text-white py-8 ">
+        <div className=" mx-0 px-4 flex flex-col md:flex-row-reverse md:justify-around justify-center ">
+
+
+          <div className="flex justify-around flex-col md:flex-row md:w-2/3 text-center md:text-left">
+            <div className=''>
               <h3 className="font-bold mb-2"></h3>
               <ul className="space-y-2">
                 <li><a href="/" className="hover:underline">Home</a></li>
@@ -21,21 +21,19 @@ const FooterComponent = () => {
                 <li><a href="/privacyPolicy" className="hover:underline">Privacy Policy</a></li>
               </ul>
             </div>
-            <div className='text-start'>
+            <div className=' mt-8'>
               <h3 className="font-bold mb-2"></h3>
               <ul className="space-y-2">
                 <li><a href="/sintana-alliance" className="hover:underline">The Sintana Alliance</a></li>
                 <li><a href="/bu-partnership" className="hover:underline">BU Partnership</a></li>
                 <li><a href="/powered-by-bu" className="hover:underline">Powered by BU</a></li>
                 <li><a href="/new-universities" className="hover:underline">New Universities</a></li>
-                <li><a href="/regional-universities" className="hover:underline">Regional Universities</a></li>
                 <li><a href="/globally-ranked-universities" className="hover:underline">Globally Ranked Universities</a></li>
               </ul>
             </div>
           </div>
 
-          {/* Left Section (Contact Information) - Normally above Right Section, but appears below on small screens */}
-          <div className="mb-8 lg:mb-0 sm:order-last lg:order-first">
+          <div className="my-8 lg:mb-0 order-last w-full md:w-1/3 text-center md:text-left">
             <h2 className="text-2xl font-bold text-yellow-500 mb-2">Sintana Education</h2>
             <address className="not-italic">
               Plot 8-11,<br />
@@ -44,7 +42,8 @@ const FooterComponent = () => {
             </address>
             <p className="mt-2">+91 9773661339</p>
           </div>
-          
+
+
         </div>
       </footer>
     </div>
--- frontend/src/pages/AsuPartnership.jsx
+++ frontend/src/pages/AsuPartnership.jsx
@@ -8,11 +8,8 @@ import Autoslider from "../components/Autoslider";
 const AsuPartnership = () => {
   return (
     <>
-
       <div className=" relative h-auto w-auto ">
-
         <div className="bg-[url('/images/BU4.png')] bg-cover bg-center h-[217.8px] w-full xl1:h-[500px] ">
-
           <Navbar />
         </div>
 
@@ -34,14 +31,10 @@ const AsuPartnership = () => {
           </div>
 
           <BUPart />
-
-          
-
-
         </div>
       </div>
 
-      <Autoslider/>
+      <Autoslider />
 
       <div className="">
         <div className="lg1:flex flex-col-reverse md1:flex sm1:flex">
@@ -50,26 +43,26 @@ const AsuPartnership = () => {
               Cutting-Edge Innovation and Research
             </h2>
             <p className="text-[15.84px] mt-4">
-              BU has consistently been ranked the number one university for
-              innovation by US News & World Report. In fact, since 2002, BU has
-              had a five-fold growth in research funding (from $100 million to
-              more than $650 million) and has become one of the fastest-growing
-              research enterprises in the United States.
+              Bennett University (BU) is recognized as No. 1 for Private
+              University Innovation in India. Known for its strong global
+              partnerships with institutions like Babson College and Georgia
+              Tech, BU leads in fostering entrepreneurship, AI, and data
+              sciences. Its No. 1 ranking for placements and No. 1 School of
+              Media & Journalism add to its reputation for academic excellence.
+              BU's rapid growth, interdisciplinary research, and industry-driven
+              programs have made it a top choice for students seeking
+              cutting-edge education and industry-ready skills.
             </p>
           </div>
 
-
           <div className=" bg-[url('/images/home-cutting-edge-photo-scaled.png')] bg-cover w-1/2 inline-block h-[668px] align-top lg1:h-[489.531px] lg1:w-auto lg1:bg-center lg1:bg-cover md1:h-[489.531px]  md1:w-auto md1:bg-center md1:bg-cover sm1:h-[489.531px] sm1:w-auto sm1:bg-center sm1:bg-cover">
             {/* <img src="/images/home-cutting-edge-photo-scaled.jpg" alt="" className='object-cover bg-no-repeat h-[668px]'/> */}
-
           </div>
         </div>
 
         <div className="sm1:flex sm1:flex-col md1:flex md1:flex-col lg1:flex lg1:flex-col">
-
           <div className=" bg-[url('/images/asu-partnership-photo.png')] bg-cover  bg-center bg-no-repeat w-1/2 inline-block h-[1234.7px] align-top lg1:h-[400px] lg1:w-auto lg1:bg-center lg1:bg-cover md1:h-[400px]  md1:w-auto md1:bg-center md1:bg-cover sm1:h-[400px] sm1:w-auto sm1:bg-center sm1:bg-cover ">
             {/* <img src="/images/home-cutting-edge-photo-scaled.jpg" alt="" className='object-cover bg-no-repeat h-[668px]'/> */}
-
           </div>
 
           <div className="py-[70px] pl-[120px] pr-[270px] w-1/2 inline-block h-[1234.7px] align-top sm1:p-6 sm1:w-full sm1:h-auto md1:p-8 md1:w-full md1:h-auto lg1:p-10 lg1:w-full lg1:h-auto">
@@ -77,27 +70,24 @@ const AsuPartnership = () => {
               World-Class Resources and Faculty
             </h2>
             <p className="text-[15.84px] mt-4">
-              BU’s 119,000 students – including more than 40,000 online
-              students — enjoy access to resources and faculty that can’t be
-              found anywhere else, including Ira A. Fulton Schools of
-              Engineering, the largest engineering program in the U.S. and the
-              W.P. Carey School of Business, one of the top business schools in
-              the country and highly ranked for academics, groundbreaking
-              research, and student outcomes. BU’s Thunderbird School of Global
-              Management is ranked #1 Worldwide for its Master of Global
-              Management. Additionally, BU is ranked among the top universities
-              in the world for research and teaching. More than 400 BU faculty
-              have been conferred by the National Academies of Sciences,
-              Engineering and Medicine with “highly prestigious” awards. Among
-              BU faculty are Nobel laureates, Pulitzer Prize winners, MacArthur
-              Foundation fellows and Fulbright American scholars.
+              Bennett University (BU) continues to receive significant
+              recognition for both its faculty and cutting-edge resources.
+              Recently, 11 of BU's faculty members were ranked among the top 2%
+              of scientists worldwide, showcasing its strong emphasis on
+              academic excellence and research. This accolade places BU among
+              India’s leading institutions for innovation and research. In
+              addition to its esteemed faculty, BU has set a milestone by
+              becoming the first private university in India to install an AI
+              Supercomputer, in collaboration with NVIDIA. This move has
+              transformed the university into a hub for advanced AI research,
+              with enhanced capabilities in fields like deep learning,
+              healthcare, and smart living solutions.
             </p>
           </div>
         </div>
       </div>
-      <Partnership/>
-      <FooterComponent/>
-
+      <Partnership />
+      <FooterComponent />
     </>
   );
 };
